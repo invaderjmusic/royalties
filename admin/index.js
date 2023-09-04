ef1c45efa0f8d1ef0b80ee91610303ac0d6a7eac
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2023-09-05 00:45:07 +0100

    refactor: Set names on load, like all other data.

--- admin/index.js
+++ admin/index.js
@@ -4,11 +4,8 @@ async function getData() {
     let res = await fetch("/api/userInfo");
     let data = await res.json();
     
-    let usernames = document.getElementsByClassName("username")
-    for (let element of usernames) {
-        element.textContent = data.username;
-    }
     datastore.username = data.username;
+    document.body.dispatchEvent(new Event("nameready"));
 }
 getData()
 
@@ -16,11 +13,20 @@ function toggleUserDropdown() {
     document.getElementById("userDropdown").classList.toggle("show");
 }
 
-window.onclick = function (e) {
+window.addEventListener("click", function (e) {
     if (!e.target.matches('.dropbtn')) {
         var myDropdown = document.getElementById("userDropdown");
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
         }
     }
-}
+})
+
+window.addEventListener("load", function (e) {
+    document.body.addEventListener("nameready", (e) => {
+        let usernames = document.getElementsByClassName("username")
+        for (let element of usernames) {
+            element.textContent = datastore.username;
+        }
+    }) 
+})
--- dashboard/index.js
+++ dashboard/index.js
@@ -1,15 +1,11 @@
+let datastore = {};
+
 async function getData() {
     let res = await fetch("/api/userInfo");
     let data = await res.json();
-    
-    let usernames = document.getElementsByClassName("username")
-    for (let element of usernames) {
-        element.textContent = data.username;
-    }
-
-    if (data.admin == true) {
-        document.getElementById("adminLink").style.display = "block";
-    }
+    datastore.username = data.username;
+    datastore.admin = data.admin;
+    document.body.dispatchEvent(new Event("nameready"));
 }
 getData()
 
@@ -17,11 +13,28 @@ function toggleUserDropdown() {
     document.getElementById("userDropdown").classList.toggle("show");
 }
 
-window.onclick = function (e) {
+window.addEventListener("click", function (e) {
     if (!e.target.matches('.dropbtn')) {
         let myDropdown = document.getElementById("userDropdown");
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
         }
     }
-}
+})
+
+window.addEventListener("load", function (e) {
+    document.body.addEventListener("nameready", (e) => {
+        let usernames = document.getElementsByClassName("username")
+        for (let element of usernames) {
+            element.textContent = datastore.username;
+        }
+    
+        if (datastore.admin == true) {
+            document.getElementById("adminLink").style.display = "block";
+        }
+        else {
+            let number = document.getElementById("userDropdownLabel").offsetWidth - parseFloat(getComputedStyle(document.getElementById("userDropdownLabel"))["padding-right"]) + 8;
+            document.getElementById("userDropdown").style.width = number.toString() + "px"
+        }
+    }) 
+})
