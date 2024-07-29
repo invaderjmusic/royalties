b1299aa24a1deee113bf55444dc715032ef085da
R2D2VaderBeef <52345992+R2D2VaderBeef@users.noreply.github.com>
2024-07-29 17:11:27 +0100

    feat(admin): Reorganise navbar into two dropdowns, for streaming and bandcamp

--- admin/index.js
+++ admin/index.js
@@ -26,9 +26,29 @@ function toggleUserDropdown() {
     document.getElementById("userDropdown").classList.toggle("show");
 }
 
+function toggleStreamingDropdown() {
+    document.getElementById("streamingDropdown").classList.toggle("show");
+}
+
+function toggleBandcampDropdown() {
+    document.getElementById("bandcampDropdown").classList.toggle("show");
+}
+
 window.addEventListener("click", function (e) {
-    if (!e.target.matches('.dropbtn')) {
+    if (!e.target.matches('#userDropdownLabel')) {
         let myDropdown = document.getElementById("userDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+    if (!e.target.matches('#streamingDropdownLabel')) {
+        let myDropdown = document.getElementById("streamingDropdown");
+        if (myDropdown.classList.contains('show')) {
+            myDropdown.classList.remove('show');
+        }
+    }
+    if (!e.target.matches('#bandcampDropdownLabel')) {
+        let myDropdown = document.getElementById("bandcampDropdown");
         if (myDropdown.classList.contains('show')) {
             myDropdown.classList.remove('show');
         }
